f24fd48cd2af5652735cd18c6d9ec067382817fc
Samson Main <ib21231@bristol.ac.uk>
2022-12-12 12:38:02 +0000

    first commit

---
+++ src/main/java/com/mains03/simplychess/SimplyChessApplication.java
@@ -0,0 +1,13 @@
+package com.mains03.simplychess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplyChessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mains03/simplychess/SimplyChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mains03.simplychess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
